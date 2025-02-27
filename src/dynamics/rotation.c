c1d51e90297c20b713b2356c6993945af0ddb16d
romanmikh <roman.mikhaylenko14@hotmail.com>
2025-02-27 17:38:09 +0000

    fix: norminette dynamics

--- src/dynamics/rotation.c
+++ src/dynamics/rotation.c
@@ -7,11 +7,8 @@ int	rotate_player(t_data *data, double rot_dir)
 	double		tmp_x;
 
 	p = &data->player;
-	rot_angle = ROTATION_ANGLE * rot_dir;  // larger angle per keystroke = faster rotation, so rot_angle functions like rotation speed in cub3d 
+	rot_angle = ROTATION_ANGLE * rot_dir;
 	tmp_x = p->dir_x;
-	// x' = x * cos(θ) - y * sin(θ)
-	// y' = x * sin(θ) + y * cos(θ)
-	// this comes from https://en.wikipedia.org/wiki/Rotation_matrix
 	p->dir_x = p->dir_x * cos(rot_angle) - p->dir_y * sin(rot_angle);
 	p->dir_y = tmp_x * sin(rot_angle) + p->dir_y * cos(rot_angle);
 	tmp_x = p->plane_x;
